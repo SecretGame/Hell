f120d3ca28d4c4c0d41bedb8af8d5dafafc97388
GGgun2013 <gggun2013@gmail.com>
2020-12-15 16:57:41 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int i=1,E=0,O=0;
+    while (i != 0)
+    {
+        cout << "Enter an integer: ";
+        cin >> i;
+         if(i%2 == 0 )
+         {
+            if(i != 0)
+            {
+         	E++;
+            }
+		 }
+         else
+         {
+            O++;
+        }
+         
+	}
+     cout << "#Even numbers = "<<E<<"\n";
+     cout << "#Odd numbers = "<<O;
     return 0;
 }
